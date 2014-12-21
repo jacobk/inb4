700c6fd72e6b40d40168cc0ed02646f87fc7eb32
jacob.kristhammar <jacob.kristhammar@mblox.com>
2014-12-22 00:52:24 +0100

    Initial database backed lastfm group scraper

--- bin/inb4.js
+++ bin/inb4.js
@@ -7,183 +7,83 @@
  * Licensed under the MIT license.
  */
 
+'use strict';
+
 var util = require('util');
-var fs = require('fs');
+var path = require('path');
 var _ = require('lodash');
 var moment = require('moment');
-var LastFmNode = require('lastfm').LastFmNode;
 var RSVP = require('rsvp');
-var RateLimiter = require('limiter').RateLimiter;
-var ProgressBar = require('progress');
+var async = require('async');
 var argv = require('minimist')(process.argv.slice(2));
+var ProgressBar = require('progress');
+
+var Storage = require('../lib/storage');
+var API = require('../lib/api');
 
 // TODO: Usage
 // from, to, key, group
 
-var from = moment(argv.from || '2014-01-01');
-var to = argv.to ? moment(argv.to) : moment();
-var lastfm = new LastFmNode({
-  api_key: argv.key
-});
-var limiter = new RateLimiter(1, 'second');
-var members;
-var stats = {};
-var baseDir = '/tmp/';
-var trackCount = 0;
+var dbPath = argv.db || '/tmp/inb4.sqlite3';
+var from = moment(argv.from || '2014-01-01').startOf('day');
+var to = (argv.to ? moment(argv.to) : moment()).startOf('day');
 
 console.log(util.format("Generating stats for period %s - %s",
   from.toISOString(), to.toISOString()));
-
-function request(method, options) {
-  return new RSVP.Promise(function(resolve, reject) {
-    lastfm.request(method, _.defaults({
-      handlers: {
-        success: function(data) {resolve(data);},
-        error: function(error) {reject(error.message);}
-      }
-    }, options));
-  });
-}
-
-function genFilePath(prefix, suffix) {
-  return util.format('%s%s-%s-%s-%s.json', baseDir, prefix, from.unix(),
-    to.unix(), suffix);
-}
-
-function writeData(prefix, suffix, data) {
-  var filePath = genFilePath(prefix, suffix);
-  console.log('Writing', filePath);
-  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-}
-
-function loadData(prefix, suffix) {
-  var filePath = genFilePath(prefix, suffix);
-  if (fs.existsSync(filePath)) {
-    return require(filePath);
-  } else {
-    return false;
-  }
-}
 
 function handleError(reason) {
   console.error('Boom goes the dynamite: ' + reason);
   console.log(reason.stack);
 }
 
-function setup() {
-  return request('group.getMembers', {group: argv.group}).then(function(data) {
-    members = _.pluck(data.members.user, 'name');
-    return true;
+RSVP.on('error', handleError);
+
+// COMMAND
+// NB. Index needs to be rebuilt if users are added/removed from group
+//
+// - build [--from ] [--to] --key --group [default from=overall] [default to=now]
+
+var storage = new Storage(dbPath);
+var api = new API(argv.key, argv.group);
+
+storage.init()
+  .then(function() {
+    api.getGroupTracks(from, to)
+      .then(function(groupTracks) {
+        var trackCount = _.reduce(groupTracks, function(acc, tracks) {
+          return acc + tracks.length;
+        }, 0);
+
+        var format = 'Adding plays [:bar] :percent';
+        var bar = new ProgressBar(format, { total: trackCount, width: 20 });
+
+        async.series(_.map(groupTracks, function(tracks, user) {
+          return function(gcb) {
+
+            console.log(' processing user', user);
+            async.series(_.chain(tracks)
+              .reject(function(track) {
+                return track['@attr'] && track['@attr'].nowplaying;
+              })
+              .map(function(track) {
+                var play = {
+                  user: user,
+                  date: parseInt(track.date.uts, 10),
+                  track: {mbid: track.mbid, name: track.name},
+                  artist: {mbid: track.artist.mbid, name: track.artist['#text']},
+                  album: {mbid: track.album.mbid, name: track.album['#text']}
+                };
+                return function(cb) {
+                  bar.tick();
+                  return storage.addPlay(play, cb);
+                };
+              })
+              .value(), gcb
+            );
+
+          };
+        }));
+
+      })
+      .catch(handleError);
   });
-}
-
-function userGetRecentTracks(user) {
-  console.log('Getting tracks for', user, '.');
-  var options = {
-    user: user,
-    limit: 200,
-    from: from.unix(),
-    to: to.unix()
-  };
-
-  function getPageCount() {
-    return request('user.getRecentTracks', options).then(function(data) {
-      return parseInt(data.recenttracks['@attr'].totalPages, 10);
-    });
-  }
-
-  function getPage(pageNumber) {
-    // console.log('Getting recent track for user', user, 'page', pageNumber);
-    return request('user.getRecentTracks', _.defaults({
-      page: pageNumber
-    }, options));
-  }
-
-  function getPages(pageCount) {
-    console.log('Found', pageCount, 'pages.');
-    return new RSVP.Promise(function(resolve, reject) {
-      var format = 'Requesting ' + user + ' tracks [:bar] :percent :etas';
-      var bar = new ProgressBar(format, { total: pageCount, width: 20 });
-      var pages = [];
-      _.times(pageCount, function(idx) {
-        limiter.removeTokens(1, function(err, remaning) {
-          pages.push(getPage(idx + 1));
-          bar.tick();
-          if (pages.length === pageCount) {
-            resolve(RSVP.all(pages));
-          }
-        });
-      });
-    });
-  }
-
-  function collateArtists(recentTracksPages) {
-    console.log('Collating artists for', user, 'found',
-        recentTracksPages.length, 'promises');
-    var artists = {}, name;
-    _.each(recentTracksPages, function(page, idx) {
-      writeData(user + 'Page', idx+1, page.recenttracks.track);
-      _.each(page.recenttracks.track, function(track) {
-        trackCount++;
-        name = track.artist['#text'];
-        if (!artists[name]) {
-          artists[name] = 1;
-        } else {
-          artists[name]++;
-        }
-      });
-    });
-
-    writeData('userArtists', user, artists);
-
-    return artists;
-  }
-
-  var cachedArtists = loadData('userArtists', user);
-
-  if (cachedArtists) {
-    console.log('Using cached artist list');
-    return RSVP.resolve(cachedArtists);
-  } else {
-    return getPageCount()
-      .then(getPages)
-      .then(collateArtists);
-  }
-}
-
-function getGroupArtists() {
-  return new RSVP.Promise(function(resolve, reject) {
-    var users = _.clone(members);
-    var artists = {};
-
-    function consumeUser() {
-      var user = users.shift();
-      if (!user) {
-        return resolve(artists);
-      }
-      userGetRecentTracks(user).then(function(userArtists) {
-        _.merge(artists, userArtists, function(a, b) {
-          return a ? a + b : b;
-        });
-        consumeUser();
-      }, reject);
-    }
-
-    consumeUser();
-  });
-}
-
-function proccessArtists(artists) {
-  writeData('groupArtists', argv.group, artists);
-  return artists;
-}
-
-
-setup()
-  .then(getGroupArtists)
-  .then(proccessArtists)
-  .then(function(artists) {
-    console.log(JSON.stringify(artists, null, 2));
-    console.log(trackCount);
-  })
-  .catch(handleError);
---
+++ db/inb4_01.sql
@@ -0,0 +1,51 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE IF NOT EXISTS tracks (
+  id INTEGER NOT NULL PRIMARY KEY,
+  mbid VARCHAR(36) NULL,
+  name VARCHAR(255) NULL,
+  artist_id INTEGER NOT NULL,
+  album_id INTEGER NOT NULL)
+
+CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER NOT NULL PRIMARY KEY,
+  name VARCHAR(255) NULL,
+  mbid VARCHAR(36) NULL)
+
+INSERT OR IGNORE INTO artists (id, name, mbid)
+  VALUES (1, 'Unknown', NULL);
+
+CREATE TABLE IF NOT EXISTS albums (
+  id INTEGER NOT NULL PRIMARY KEY,
+  name VARCHAR(255) NULL,
+  mbid VARCHAR(36) NULL,
+  releasedate INTEGER NULL,
+  artist_id INTEGER NOT NULL)
+
+INSERT OR IGNORE INTO albums (id, name, mbid, releasedate, artist_id)
+  VALUES (1, 'Unknown', NULL, NULL, 1);
+
+CREATE TABLE IF NOT EXISTS tracks (
+  id INTEGER NOT NULL PRIMARY KEY,
+  mbid VARCHAR(36) NULL,
+  name VARCHAR(255) NULL,
+  artist_id INTEGER NOT NULL,
+  album_id INTEGER NOT NULL)
+
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER NOT NULL PRIMARY KEY,
+  name VARCHAR(45) NULL)
+
+CREATE TABLE IF NOT EXISTS plays (
+  id INTEGER NOT NULL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  track_id INTEGER NOT NULL,
+  date INTEGER NULL)
+
+CREATE TABLE IF NOT EXISTS imports (
+  id INTEGER NOT NULL PRIMARY KEY,
+  from_time DATETIME NULL,
+  to_time DATETIME NULL,
+  start_time DATETIME NULL,
+  stop_time DATETIME NULL,
+  status VARCHAR(10) NOT NULL DEFAULT 'started')
